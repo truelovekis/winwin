17bf93db8224066f94c09c5fc35ad17aa6b73d4c
hyunowkd <hyunowkd@naver.com>
2023-06-20 18:06:09 +0900

    no message

--- src/main/java/com/example/winwin/controller/board/CommunityController.java
+++ src/main/java/com/example/winwin/controller/board/CommunityController.java
@@ -1,7 +1,16 @@
 package com.example.winwin.controller.board;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/mentormentee/*")
 public class CommunityController {
+    @GetMapping("/mentorProfile")
+    public String mentorProfile(){
+        return "mentormentee/mentorProfile";
+    }
 }
