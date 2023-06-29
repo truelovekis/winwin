eac7cc58b4c3cfadc2d410bf4623b48f3452b59c
KimByoungKoo <qudrn3228@gmail.com>
2023-06-29 15:47:12 +0900

    share Commit

--- src/main/java/com/example/winwin/mapper/user/UserMapper.java
+++ src/main/java/com/example/winwin/mapper/user/UserMapper.java
@@ -1,7 +1,15 @@
 package com.example.winwin.mapper.user;
 
+import com.example.winwin.dto.user.UserDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UserMapper {
+
+//    회원가입
+    void insert(UserDto userDto);
+
+//    로그인
+    Long selectUserNumber(@Param("userId")String userId, @Param("userPassword")String userPassword);
 }
