7e2dd3131021e5fe3c26b6a2cd23ef87ed9d9d8d
hyunowkd <hyunowkd@naver.com>
2023-07-19 20:28:01 +0900

    로그인 인터셉터 모달창띄우기 도전

--- src/main/java/com/example/winwin/interceptor/LoginInterceptor.java
+++ src/main/java/com/example/winwin/interceptor/LoginInterceptor.java
@@ -13,6 +13,7 @@ public class LoginInterceptor implements HandlerInterceptor {
         Object userNumber = request.getSession().getAttribute("userNumber");
 
         if(userNumber == null){
+            request.setAttribute("checkLogin", false);
             response.sendRedirect("/main/main");
 //            이전 페이지로 이동 어캐 함?
             return false;
