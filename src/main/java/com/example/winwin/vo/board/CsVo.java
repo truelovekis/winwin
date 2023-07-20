ff957d263cf8c0859e8bc7afc3d130a1b7c6a134
dnjsckdtlr <dx9139@naver.com>
2023-07-20 17:49:16 +0900

    casdfaeqfrq

--- src/main/java/com/example/winwin/vo/board/CsVo.java
+++ src/main/java/com/example/winwin/vo/board/CsVo.java
@@ -15,11 +15,12 @@ public class CsVo extends Criteria {
     private String csContent;
     private String csDate;
     private Long userNumber;
-    private String gradeName;
     private String userId;
     private String userNickname;
     private String userBelong;
     private String userPosition;
+    private String userStatus;
+    private String gradeName;
     private int likeCnt; // 좋아요 추가함
     private int csReadCnt; // 조회수 추가함
     private int commentCnt; // 댓글수 추가함
